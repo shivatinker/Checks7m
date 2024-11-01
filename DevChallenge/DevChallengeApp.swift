40e639072c4848862a1c18c8e240b01297132c2d
Andrii Zinoviev <azinoviev@readdle.com>
2024-11-01 21:47:50 +0100

    Initial Commit

---
+++ DevChallenge/DevChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevChallengeApp.swift
+//  DevChallenge
+//
+//  Created by Andrii Zinoviev on 01.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DevChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
